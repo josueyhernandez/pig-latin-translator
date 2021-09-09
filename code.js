081583cdf58ddfa0973364c49277759035fe6c09
Josue Hernandez <josueyhernandez18@gmail.com>
2021-09-09 16:46:06 -0400

    Use function for intermediate challenge

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 let punctuations = [".", ",", "?", "!", "#", ";"]
-let extraChar = [1, 2, 3, 4, 5, 6, 7, 8, 9, " "]
-let punctuationValue
 
 function beginVowel(word) {
    if (vowels.indexOf(word[0]) !== -1) {
@@ -23,11 +21,11 @@ function beginConsonant(word) {
         } 
         consonant += character
     }
-    return word.substring(consonant.length) + consonant + "ay"
+    return word.substring(consonant.length) + "-" + consonant + "ay"
 }
 beginConsonant("thing")
-console.assert(beginConsonant("bite") === "itebay", "Function doesn't work")
-console.assert(beginConsonant("thing") === "ingthay", "Function doesn't work")
+console.assert(beginConsonant("bite") === "ite-bay", "Function doesn't work")
+console.assert(beginConsonant("thing") === "ing-thay", "Function doesn't work")
 
 function checkWords(word) {
     if (vowels.indexOf(word[0]) !== -1) {
@@ -37,20 +35,22 @@ function checkWords(word) {
     }  
 }
 checkWords("one")
-console.assert(checkWords("trash") === "ashtray", "Function doesn't turn word to Pig Latin")
+console.assert(checkWords("trash") === "ash-tray", "Function doesn't turn word to Pig Latin")
 console.assert(checkWords("every") === "every-yay", "Function doesn't turn word to Pig Latin")
 
 function checkSentence(words) {
-    let multipleWords = words.split(" ")
-    multipleWords.map(word => {
-        checkWords(word)
-    })
+    return words
+    .split(" ")
+    .map((word) => checkWords(word))
     .join(" ")
-    return checkWords(words)
+    .replaceAll(".", "")
+    .replaceAll(",", "")
+    .replaceAll("?", "")
+    .replaceAll("!", "")
 }
 checkSentence("Hello how are you")
-console.assert(checkSentence("hey") === "eyhay", "Function cannot handle one word sentence")
-console.assert(checkSentence("how are you") === "owhay are-yay ouyay", "Function cannot handle multiple words")
+console.assert(checkSentence("hey") === "ey-hay", "Function cannot handle one word sentence")
+console.assert(checkSentence("how are you") === "ow-hay are-yay ou-yay", "Function cannot handle multiple words")
 
 
 let textarea = document.getElementById("text")
@@ -59,24 +59,6 @@ let pigWord = document.getElementById("pig-word")
 textarea.addEventListener("keyup", (e) => {
     let text = e.target.value
     let words = text
-        .split(" ")
-        .map((word) => {
-        if (vowels.indexOf(word[0]) !== -1) {
-            return word + "-yay"
-        }
-            let consonant = ""
-            for (let i = 0; i < word.length; i++) {
-                let character = word[i]
-                if (vowels.includes(character)) {
-                    break
-                } 
-                consonant += character
-            }
-
-            return word.substring(consonant.length) + consonant + "ay"
-        
-    })
-    .join(" ")
-    pigWord.innerText = words
+    pigWord.innerHTML = checkSentence(words)
 })
 
